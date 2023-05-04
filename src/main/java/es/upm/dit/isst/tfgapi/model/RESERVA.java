a8471eff5e03e5077e47305d665772cfa5cb5b43
jaimeberzal <123977181+jaimeberzal@users.noreply.github.com>
2023-05-04 10:18:53 +0200

    Add files via upload

--- src/main/java/es/upm/dit/isst/tfgapi/model/RESERVA.java
+++ src/main/java/es/upm/dit/isst/tfgapi/model/RESERVA.java
@@ -12,12 +12,14 @@ public class RESERVA {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String idreserva;
     private String fechayhora;
+    private String espacioreservado;
     private Boolean estaReservada;
 
     public RESERVA () { }
-    public RESERVA(String idreserva, String fechayhora, Boolean estaReservada) {
+    public RESERVA(String idreserva, String fechayhora,String espacioreservado, Boolean estaReservada) {
         this.idreserva = idreserva;
         this.fechayhora = fechayhora;
+        this.espacioreservado = espacioreservado;
         this.estaReservada = estaReservada;
     }
     public String getIdreserva() {
@@ -31,6 +33,12 @@ public class RESERVA {
     }
     public void setFechayhora(String fechayhora) {
         this.fechayhora = fechayhora;
+    }
+    public String getEspacioreservado() {
+        return espacioreservado;
+    }
+    public void setEspacioreservado(String espacioreservado) {
+        this.espacioreservado = espacioreservado;
     }
     public Boolean getEstaReservada() {
         return estaReservada;
